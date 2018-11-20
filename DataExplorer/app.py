c839a26e36f41f21d0e879ee4c3ea55236d7e751
Gijs van den Abeele <gijsvdabeele@gmail.com>
2018-11-21 00:18:08 +0100

    Initial files. Added dataexplorer and filesorter scripts.

---
+++ DataExplorer/app.py
@@ -0,0 +1,148 @@
+# pyinstaller --onefile --distpath  app_json.py
+import json
+import time
+import os
+from datetime import datetime
+files_dir = "files/"
+files = []
+output_file = "output.txt"
+
+for file in os.listdir(files_dir):
+    files.append(file)
+print("Please choose option by entering the corresponding number:\n"
+      "1. Count occurrences of term\n"
+      "2. List occurrences of term\n"
+      "3. List comments from username\n"
+      "4. Count total number of lines")
+action = input()
+
+if action == "1":
+    # Defining vars for action
+    print("Please enter search term")
+    inp = input()
+    count = 0
+    start = time.time()
+
+    # Going through all files in specified directory
+    for file in os.listdir(files_dir):
+        filename = str(file)
+        print("Reading file " + str(files.index(filename)+1) + "/" + str(len(files)))
+
+        # Open the file
+        with open(files_dir+filename) as data:
+            # Read each fow
+            for row in data:
+                data = json.loads(row)
+                try:
+                    if inp in data["body"]:
+                        count += 1
+                except UnicodeDecodeError:
+                    print("Bad row:"+row)
+    print("Total count is " + str(count))
+    end = time.time()
+    print("Done in " + str(round(end - start)) + " seconds.")
+    input()
+
+if action == "2":
+    # Defining vars for action
+    print("Please enter search term")
+    inp = input()
+    # Check if should output to file
+    print("1. Print results to console\n2. Print to file")
+    out_format = input()
+    if out_format == "2":
+        ttime = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        ttime = ttime.replace(":", "-").replace(" ", "_")
+        output_file = open(str(ttime)+".txt", "a+")
+    start = time.time()
+
+    # Going through all files in specified directory
+    for file in os.listdir(files_dir):
+        filename = str(file)
+        print("Reading file " + str(files.index(filename) + 1) + "/" + str(len(files)))
+
+        # Open the file
+        with open(files_dir+filename) as data:
+            # Read each fow
+            for row in data:
+                data = json.loads(row)
+                try:
+                    if inp in data["body"]:
+                        if out_format == "2":
+                            try:
+                                output_file.write(data["body"]+"\n")
+                                output_file.write("-"+data["author"]+"\n")
+                                output_file.write("----------------------\n")
+                            except UnicodeEncodeError:
+                                continue
+                        else:
+                            print(data["body"])
+                            print("-"+data["author"] + "\n")
+                            print("----------------------")
+                except UnicodeDecodeError:
+                    print("Bad row:"+row)
+    end = time.time()
+    print("Done in " + str(round(end - start)) + " seconds.")
+    input()
+
+if action == "3":
+    # Defining vars for action
+    print("Please enter username")
+    inp = input()
+    # Check if should output to file
+    print("1. Print results to console\n2. Print to file")
+    out_format = input()
+    if out_format == "2":
+        out_format = True
+        ttime = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        ttime = ttime.replace(":", "-").replace(" ", "_")
+        output_file = open(str(ttime) + ".txt", "a+")
+    start = time.time()
+
+    # Going through all files in specified directory
+    for file in os.listdir(files_dir):
+        filename = str(file)
+        print("Reading file " + str(files.index(filename) + 1) + "/" + str(len(files)))
+
+        # Open the file
+        with open(files_dir+filename) as data:
+            # Read each fow
+            for row in data:
+                data = json.loads(row)
+                try:
+                    if inp == data["author"]:
+                        if out_format == "2":
+                            try:
+                                output_file.write(data["body"] + "\n")
+                                output_file.write("----------------------\n")
+                            except UnicodeEncodeError:
+                                continue
+                        else:
+                            print(data["body"])
+                            print("----------------------")
+                except UnicodeDecodeError:
+                    print("Bad row:"+row)
+    end = time.time()
+    print("Done in " + str(round(end - start)) + " seconds.")
+    input()
+
+if action == "4":
+    # Defining vars for action
+    count = 0
+    start = time.time()
+
+    # Going through all files in specified directory
+    for file in os.listdir(files_dir):
+        filename = str(file)
+        print("Reading file " + str(files.index(filename)+1) + "/" + str(len(files)))
+
+        # Open the file
+        with open(files_dir+filename) as data:
+            # Read each fow
+            for row in data:
+                count += 1
+        print("Current number of lines is "+str(count))
+    print("Total number of lines is " + str(count))
+    end = time.time()
+    print("Done in " + str(round(end - start)) + " seconds.")
+    input()
---
+++ FileSorter/FileSorter.py
@@ -0,0 +1,16 @@
+import os.path
+dir = input()
+
+# Goes through each file in directory
+for file in os.listdir(dir):
+    # Check file type
+    ext = os.path.splitext(str(file))[1]
+    ext = ext[1:]
+    # Skips .py and folders
+    if ext != "py" and os.path.isdir(str(file)) is False:
+        # Checks if folder already exist if not makes it
+        if os.path.isdir(dir+ext):
+            os.rename(dir+str(file), dir+ext+"/"+str(file))
+        else:
+            os.makedirs(dir+ext)
+            os.rename(dir + str(file), dir+ext+"/"+str(file))
