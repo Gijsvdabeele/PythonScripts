ae7abedb9cf5d210e53bc7e2fb2d00a7bcd4ca83
Gijs van den Abeele <gijsvdabeele@gmail.com>
2018-11-21 17:33:59 +0100

    Clean up for filesorter. Made method for repeat actions and changed code format according to PEP8 standards

--- FileSorter/FileSorter.py
+++ FileSorter/FileSorter.py
@@ -1,17 +1,22 @@
+# This script makes a folder for every type of file in the directory
+# and moves every file to its corresponding folder.
 import os.path
+DIR = str(os.path.dirname(os.path.realpath(__file__)))+"/"
 
-dir = str(os.path.dirname(os.path.realpath(__file__)))+"/"
+
+def move_file():
+    os.rename(DIR + str(file), DIR + ext + "/" + str(file))
+
 
 # Goes through each file in directory
-for file in os.listdir(dir):
-    # Check file type
-    ext = os.path.splitext(str(file))[1]
-    ext = ext[1:]
-    # Skips .py and folders
-    if ext != "py" and os.path.isdir(str(file)) is False:
+for file in os.listdir(DIR):
+    # Skips self and folders
+    if str(file) != os.path.basename(__file__) and os.path.isdir(str(file)) is False:
+        # Check file type
+        ext = os.path.splitext(str(file))[1][1:]
         # Checks if folder already exist if not makes it
-        if os.path.isdir(dir+ext):
-            os.rename(dir+str(file), dir+ext+"/"+str(file))
+        if os.path.isdir(DIR+ext):
+            move_file()
         else:
-            os.makedirs(dir+ext)
-            os.rename(dir + str(file), dir+ext+"/"+str(file))
+            os.makedirs(DIR+ext)
+            move_file()
